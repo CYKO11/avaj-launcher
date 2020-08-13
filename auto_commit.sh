90763fb67a2fac33f0facf9a45a863051af21f70
liamkrieling <lmk310500@gmail.com>
2020-08-13 09:26:02 +0200

    fixed auto_commit script

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,8 +2,9 @@
 git add .
 git status
 git commit -m "$1"
-read -s -N 1 -t 1 key
-if [[ $key != $'\x0a' ]];        # if input == ENTER key
-then
-exit 0
-fi
+read -n 1 -s
+echo "done"
+# if [[ $key != $'\x0a' ]];        # if input == ENTER key
+# then
+# exit 0
+# fi
