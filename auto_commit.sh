9e413d0dd425c72e248ab3880286b614ac1d1fe4
liamkrieling <lmk310500@gmail.com>
2020-08-13 09:32:44 +0200

    fixed auto_commit script

--- auto_commit.sh
+++ auto_commit.sh
@@ -4,7 +4,7 @@ git status
 git commit -m "$1"
 echo "Press enter to push or any key to cancel push"
 read -n 1 -s key
-if [[ $key != $'\x0a' ]];        # if input == ENTER key
+if [[ $key == $'\x0a' ]];        # if input == ENTER key
 then
 echo "pushing ......"
 git push
